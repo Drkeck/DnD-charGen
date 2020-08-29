9ecc2585f26086d69a2180cbfbec959de7836c37
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2020-08-29 12:36:12 -0700

    added a user schema and some of the basic file structure to the existing server section

---
+++ server/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    characters: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: '5e'
+        },
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'p1'
+        }
+    ]
+})
+
+userSchema.pre('save', async function(next) {
+    if(this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds)
+    }
+
+    next()
+})
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

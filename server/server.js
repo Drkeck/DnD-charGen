78cb0db34c75e4295b158b3fa48e3796b43c5bf2
Drkeck <k3ckl3y.a@gmail.com>
2020-11-29 18:19:12 -0800

    added

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 // const { authMiddleware } = require('./utils/Auth');
 const { typeDefs, resolvers } = require('./schema');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const server = new ApolloServer({

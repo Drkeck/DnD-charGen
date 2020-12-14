7d8624697a5a5cb08e9d44bc894a4243b7f120bd
Drkeck <k3ckl3y.a@gmail.com>
2020-12-14 09:12:05 -0800

    added the ability to modify stats in the front end when generating a new character and a tracking of what the modifier for the characters stats would be

--- client/src/Components/Stats/index.js
+++ client/src/Components/Stats/index.js
@@ -6,22 +6,20 @@ function Stats (props) {
         setCurrentStats
     } = props;
    async function handleChange(event) {
-        console.log(event.target)
+        const n = event.target.name
         const i = event.target.id
         const v = event.target.value
-        console.log(i, v);
-        console.log(attributes[i].score)
-        setCurrentStats(attributes[i],{
-            name: event.target.name,
-            score: v
-        })
+        let newArry = [...attributes];
+        newArry[i].score = v
+        setCurrentStats(newArry)
     }
 return (
     <div>
         {attributes.map((attribute, num) => (
         <form key={num}>
             <label htmlFor={attribute.name}>{attribute.name}</label>
-            <input id={num} name={attribute.name} type='number' defaultValue={attribute.score} onChange={handleChange}/>
+            <input id={num} name={attribute.name} type='number' value={attribute.score} onChange={handleChange}/>
+        <p>{Math.floor((attribute.score - 10) / 2)}</p>
         </form>
         ))}
     

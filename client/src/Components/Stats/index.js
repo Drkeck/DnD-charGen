da04d86537f19d17e2431eb9fd3ab50e8f487aae
Drkeck <k3ckl3y.a@gmail.com>
2020-12-17 00:58:32 -0800

    added the ability to register a +2 modifer for the races that have to add their bonus to any stat are able to do so without scewing the entire stat sheet

--- client/src/Components/Stats/index.js
+++ client/src/Components/Stats/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Stats (props) {
     const {
@@ -6,9 +6,12 @@ function Stats (props) {
         setCurrentStats,
         CurrentRace
     } = props;
+    const [isMod, setMod] = useState(true);
+
+    const modCan = CurrentRace.name === "Human" || CurrentRace.name === "Half-elf" || CurrentRace.name === "Half-orc";
 
 
-   function handleChange(event) {
+    function handleChange(event) {
         const i = event.target.id
         const v = parseInt(event.target.value)
         let newArry = [...attributes];
@@ -17,13 +20,35 @@ function Stats (props) {
 
     }
 
+    function handleCheck(event) {
+        event.preventDefault();
+        const i = parseInt(event.target.value)
+        console.log(i)
+        let v;
+        let b;
+        if (i > 9) {
+            v = i - 10
+            b = 2
+            setMod(false)
+        } else {
+            v = i + 10
+            b = 0
+            setMod(true)
+        }
+        console.log(i, v)
+        let newArry = [...attributes];
+        newArry[v].rBonus = b
+        setCurrentStats(newArry)
+    }
+
 return (
     <div>
         {attributes.map((attribute, num) => (
         <form key={num}>
             <label htmlFor={attribute.name}>{attribute.name}: </label>
             <input id={num} name={attribute.name} type='number' value={attribute.score} onChange={handleChange}/>
-            <p>{attribute.rBonus}</p>
+        { !isMod && modCan && attribute.rBonus === 2 ? <button onClick={handleCheck} value={num - 10}>Remove +2</button>: <></>}
+        { isMod && modCan ? <button onClick={handleCheck} value={num + 10}>add +2</button> : <p>Racial Bonus: {attribute.rBonus}</p>}
         <p>Total: {attribute.score + attribute.rBonus}</p>
         <p>Modifier: {Math.floor(((attribute.score + attribute.rBonus) - 10) / 2)}</p>
         </form>
